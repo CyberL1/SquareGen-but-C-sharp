d6098a660db6f1bf175f8afb575e68d0253f6203
CyberL1 <mateusz.hf18@gmail.com>
2023-12-06 14:39:53 +0100

    Dodaj pliki projektów.

---
+++ SquareGen-but-C-sharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SquareGen_but_C_sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Provide height");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Provide width");
+            int height = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
